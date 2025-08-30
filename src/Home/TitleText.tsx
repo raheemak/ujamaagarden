98cbdf61881e512b31ebd06ee774025ff6a37d9d
Raheema Kadwa <raheema@Raheemas-MacBook-Pro.local>
2025-08-29 20:08:49 -0400

    tryinng to make responsive again

--- src/Home/TitleText.tsx
+++ src/Home/TitleText.tsx
@@ -4,7 +4,7 @@ export const TitleText = () => {
       <div className="absolute inset-10 top-50 left-0 right-0  bg-[#8b9b3c] opacity-20 blur-2xl z-0 [clip-path:polygon(10%_85%,90%_45%,95%_60%,50%_80%,5%_60%)]"></div>
 
       <h1
-        className="pt-[130px] text-[120px] sm:text-[180px] md:text-[220px] lg:text-[280px] font-cursive font-extrabold text-transparent 
+        className="pt-[130px] text-[120px] sm:text-[100px] md:text-[220px] lg:text-[280px] font-cursive font-extrabold text-transparent 
                    bg-clip-text bg-cover bg-center [-webkit-text-stroke:2px_#000] leading-none 
                    bg-[url('/trees-3.jpg')]"
       >
